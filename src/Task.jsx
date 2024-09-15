56db31ba790a99c295b212fa0ad7ae76b2ed04f7
aj <balajivijay679@gmail.com>
2024-09-16 00:20:54 +0530

    done

---
+++ src/Task.jsx
@@ -0,0 +1,22 @@
+
+import "./Task.css";
+function Task({content,del,hup,hdo,index}){
+
+    return(
+        <div className="task">
+        <div>{++index}</div>
+            <div>
+            <p> {content}</p>
+                
+            </div>
+            <div className="taskbtns">
+                <button onClick={()=>del()} className="btn del">❌</button>
+                <button onClick={()=>hup()} className="btn up"> ☝️</button>
+                <button onClick={()=>hdo()} className="btn down">👇</button>
+
+            </div>
+        </div>
+    )
+}
+
+export default Task;
---
+++ src/Todo.jsx
@@ -0,0 +1,71 @@
+
+import "./Todo.css"
+import Task from "./Task";
+import { useState } from "react";
+function Todo(){
+
+    let [tasks,setTasks]=useState(["eat","sleep",]);
+
+    let [task,setTask]=useState("");
+
+    function handleTask(e){
+        setTask(e.target.value);
+
+    }
+
+    function handleAddTask(){
+        if(task!=""){
+            setTasks([...tasks,task]);
+            setTask("")
+        }
+    }
+
+    function deleteTask(index){
+        setTasks([...tasks.filter((_,i)=>i!=index)]);
+    }
+
+    function swap(arr,a,b){
+        return [arr[a], arr[b]] = [arr[b], arr[a]];
+    }
+
+    function handleUp(a){
+        let b=a-1;
+        if(a>0){
+            swap(tasks,a,b);
+            setTasks([...tasks])
+        }
+    }
+
+
+    function handleDown(a){
+        let b=a+1;
+        if(a<tasks.length-1){        
+            swap(tasks,a,b);
+            setTasks([...tasks])
+        }
+    }
+    return(
+        <div className="mainContainer">
+           <center> <h1>T0DO-LIST</h1></center>
+           <main>
+             <div className="ip">
+                <input onChange={handleTask} value={task} placeholder="Enter a task" />
+                <button onClick={handleAddTask} className="add">Add</button>
+             </div>
+                <div className="allTasks">
+                {tasks.length && tasks.map((a,i)=><Task  
+                                 hdo={()=>handleDown(i)} 
+                                 hup={()=>handleUp(i)} 
+                                 del={()=>{deleteTask(i)}} 
+                                 key={i} 
+                                 index={i}
+                                 content={a.toUpperCase()}  
+                                 />)||
+                                 <center><h2 style={{color:"black"}}>NO TASKS LEFT</h2></center>}
+                </div>
+           </main>
+        </div>
+    )
+}
+
+export default Todo;
